179aaf7d66318878940be93cb848019e95b166af
mariya <mariya@vuapex.com>
2020-11-01 19:53:23 +0500

    to 8

--- client/src/App.js
+++ client/src/App.js
@@ -4,16 +4,19 @@ import './App.css';
 import AuthForm from './components/auth/AuthForm';
 import { AuthContext } from './components/auth/auth';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import CustomAppBar from './components/appbar/AppBar';
+import PrivateRoute from './components/route_types/PrivateRoute';
+import HomePage from './components/homepage/Home';
 
 function App() {
 
   const existingToken = localStorage.getItem("token") || "";
   const existingUsername = localStorage.getItem("username") || "";
-  const [authToken, setAuthToken] = useState(existingToken);
-  const [username, setUsername] = useState(existingUsername);
+  const [ authToken, setAuthToken ] = useState(existingToken);
+  const [ username, setUsername ] = useState(existingUsername);
 
   const setUserName = (data) => {
-    if (!data) {
+    if(!data) {
       localStorage.removeItem('username');
       setUsername();
     } else {
@@ -23,7 +26,7 @@ function App() {
   }
 
   const setToken = (data) => {
-    if (!data) {
+    if(!data) {
       localStorage.removeItem('token');
       setAuthToken();
     } else {
@@ -33,19 +36,20 @@ function App() {
   }
 
   return (
-    <AuthContext.Provider value={{ authToken, setAuthToken: setToken, username, setUserName: setUserName }}>
+    <AuthContext.Provider value={{authToken, setAuthToken: setToken, username, setUserName: setUserName}}>
       <div className='App'>
         <BrowserRouter>
+          <CustomAppBar/>
           <Switch>
+            <Route exact path="/" component={AuthForm} />
             <Route exact path="/login" component={AuthForm} />
             <Route exact path="/signup" component={AuthForm} />
+            <PrivateRoute exact path="/home" component={HomePage} />
           </Switch>
-      
-
         </BrowserRouter>
-      </div>
+       </div>
     </AuthContext.Provider>
-
+    
   )
 
 }
